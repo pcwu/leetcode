238d7172b271ffbc703a1f2f7a6b6309645c9dbb
Po-Chun Wu <sb1126@gmail.com>
2016-11-18 18:01:27 +0800

    412. Fizz Buzz

--- 412-FizzBuzz/FizzBuzz.py
+++ 412-FizzBuzz/FizzBuzz.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         result = []
 
-        for i in xrange(1,n+1):
+        for i in xrange(1, n+1):
             if i % 15 == 0:
                 result.append("FizzBuzz")
             elif i % 5 == 0:
@@ -15,5 +15,4 @@ class Solution(object):
                 result.append("Fizz")
             else:
                 result.append(str(i))
-
         return result
