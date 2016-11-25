8a6a8d39745ceccbfa6422da1e8f4ffa39028d71
Po-Chun Wu <sb1126@gmail.com>
2016-11-25 20:58:05 +0800

    014. longest Common Prefix AC, PR 91

---
+++ 014-longest-common-prefix/longestCommonPrefix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        result = strs[0]
+        for i in xrange(len(strs)):
+            j = 0
+            while j < len(result) and j < len(strs[i]) and result[j] == strs[i][j]:
+                    j = j + 1
+
+            result = result[0:j]
+
+            if len(result) == 0:
+                return ""
+        return result
